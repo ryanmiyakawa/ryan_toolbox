d3f093525e5707407ebe73604f7f5efea552c91f
Ryan-Miyakawa <ryan@hooktheory.com>
2023-05-23 13:13:32 -0700

    adding gsheet getter

---
+++ getGSheet.m
@@ -0,0 +1,18 @@
+function sheet_data = getGSheet(gID, sheet_name)
+
+
+url_name = sprintf('https://docs.google.com/spreadsheets/d/%s/gviz/tq?tqx=out:csv&sheet=%s',...
+    gID, sheet_name);
+
+sheet_data = webread(url_name);
+
+
+for i = 1:numel(sheet_data)
+    value = sheet_data{i};
+    
+    if isnumeric(value) && isnan(value)
+        sheet_data{i} = '';
+    elseif isnumeric(value)
+        sheet_data{i} = sprintf('<%g>', value);
+    end
+end
